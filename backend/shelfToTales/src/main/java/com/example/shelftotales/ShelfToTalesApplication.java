dd69c57c8b3c5adb2b98bda649b72cdffe098fb7
pacman-cli <puspopuspo520@gmail.com>
2026-05-10 13:53:37 +0600

    Initial project setup

---
+++ backend/shelfToTales/src/main/java/com/example/shelftotales/ShelfToTalesApplication.java
@@ -0,0 +1,13 @@
+package com.example.shelftotales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShelfToTalesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShelfToTalesApplication.class, args);
+    }
+
+}
---
+++ backend/shelfToTales/src/test/java/com/example/shelftotales/ShelfToTalesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shelftotales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShelfToTalesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
